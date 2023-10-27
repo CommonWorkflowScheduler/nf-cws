74936a88a058fb6425f4fe6c2ed0cff37df8879e
Florian Friederici <friederici@gmx.de>
2023-10-27 16:02:20 +0200

    restore changes

--- launch.sh
+++ launch.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-
-export NXF_PLUGINS_DEV=$SCRIPT_DIR/plugins
-$SCRIPT_DIR/../nextflow/launch.sh "$@"
+export NXF_PLUGINS_DEV=$PWD/plugins
+ ../nextflow/launch.sh "$@"
